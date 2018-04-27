3d0597664c81010faab4110a5fe0435ba7ac77cc
María Auxiliadora Rodríguez Torres <mrodriguezt@santoscmi.com>
2018-04-27 11:33:08 -0500

    Subir a IFS Facturas Proveedores-Validar Facturas Existentes

--- resources/views/facturas/layoutXML.blade.php
+++ resources/views/facturas/layoutXML.blade.php
@@ -1,15 +1,15 @@
 <Grid>
-    <Cfg id='reporteXML' Code='PTAEXXOQBFRYSC' Sort='razonSocial' MaxHeight='1' MinTagHeight="400" ShowDeleted="1" Deleting="0" DateStrings='1'
+    <Cfg id='reporteXML' Style='Light' Code='PTAEXXOQBFRYSC' Sort='razonSocial' MaxHeight='1' MinTagHeight="400" ShowDeleted="1" Deleting="0" DateStrings='1'
          IdNames='Project'  AppendId='1' FullId='1' IdChars='0123456789' Selecting='0' NumberId='1' LastId='1' CaseSensitiveId='1'/>
 
     <Cols>
-        <C Name='razonSocial' Width='200' Type='Text' CanEdit='0'/>
+        <C Name='razonSocial' Width='200' Type='Lines' CanEdit='0'/>
         <C Name='ruc' Width='100' Type='Text' CanEdit='0'/>
-        <C Name='claveAcceso' Width='200' Type='Text' CanEdit='0'/>
-        <C Name='codDoc' Width='80' Type='Text' CanEdit='0'/>
-        <C Name='no_factura' Width='200' Type='Text' CanEdit='0'/>
-        <C Name='enviado_ifs' Width='200' Type='Text' CanEdit='0'/>
-        <C Name='fecha_envio' Width='200' Type='Text' CanEdit='0'/>
+        <C Name='claveAcceso' Width='335' Type='Lines' CanEdit='1'/>
+        <C Name='codDoc' Width='80' Type='Text' CanEdit='0' Align="center"/>
+        <C Name='no_factura' Width='150' Type='Text' CanEdit='0'/>
+        <C Name='enviado_ifs' Width='100' Type='Text' CanEdit='0'/>
+        <C Name='fecha_envio' Width='100' Type='Text' CanEdit='0'/>
         <C Name='path' Width='200' Type='Link' CanEdit='0'/>
     </Cols>
     <Header  Wrap="1"
