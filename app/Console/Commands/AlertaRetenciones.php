13f1ab40281a46d442fb03b4972568e59127e82d
María Auxiliadora Rodríguez Torres <mrodriguezt@santoscmi.com>
2019-10-18 09:04:16 -0500

    Alertas Retenciones IAA

--- app/Console/Commands/AlertaRetenciones.php
+++ app/Console/Commands/AlertaRetenciones.php
@@ -41,7 +41,7 @@ class AlertaRetenciones extends Command
     {
         $facturasSinRetencion = array();
         $facturasSinAutorizacion = array();
-        $facturas = \DB::connection('oracle')->table('MAN_SUPP_INVOICE')
+        /* $facturas = \DB::connection('oracle')->table('MAN_SUPP_INVOICE')
             ->where('INVOICE_DATE','>=','2018-01-01')
             ->where('SERIES_ID','!=','SI')
             ->where('SERIES_ID','!=','41')
@@ -71,7 +71,7 @@ class AlertaRetenciones extends Command
 
         }
 
-        /*if(count($facturasSinRetencion)>0 || count($facturasSinAutorizacion)>0) {
+       if(count($facturasSinRetencion)>0 || count($facturasSinAutorizacion)>0) {
             Mail::to('jleon@santoscmi.com')->send(new EnviarAlertaRetenciones($facturasSinAutorizacion, $facturasSinRetencion));
             Mail::to('rvasconez@santoscmi.com')->send(new EnviarAlertaRetenciones($facturasSinAutorizacion, $facturasSinRetencion));
             Mail::to('rmorenol@santoscmi.com')->send(new EnviarAlertaRetenciones($facturasSinAutorizacion, $facturasSinRetencion));
